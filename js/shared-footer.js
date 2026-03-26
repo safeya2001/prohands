bbbc10d89fed8fad01727527b13944ab6d5551ba
Safeya <s.Isteit@dopatravelapp.com>
2026-03-26 12:11:48 +0300

    Fix: update footer email to ProHands.Training@protraining.org

--- js/shared-footer.js
+++ js/shared-footer.js
@@ -46,7 +46,7 @@ class SharedFooter extends HTMLElement {
               </li>
               <li>
                 <svg viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" stroke="currentColor"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg>
-                <a href="mailto:Professionalhandsfortraining@gmail.com">Professionalhandsfortraining@gmail.com</a>
+                <a href="mailto:ProHands.Training@protraining.org">ProHands.Training@protraining.org</a>
               </li>
             </ul>
           </div>
